a588da764cff5e714913532f0cfdfddd225a7e60
JhonDeibySilvaRojasADSO <silvarojasjhondeiby@gmail.com>
2022-09-21 10:57:26 -0500

    ejercicio 2

--- java.js
+++ java.js
@@ -1,10 +1,12 @@
 addEventListener("DOMContentLoaded", ()=>{
-    let num=Number(prompt(`ingrese el numero: `))
-    if (num>0 && num<100){
-        console.log(`el numero :${num} era menor que 100`)
+    let num1=Number(prompt("ingrese el primer numero "))
+    let num2=Number(prompt("ingrese el segundo numero "))
+    let num3=Number(prompt("ingrese el tercer numero "))
+    if (num1>num2 && num1>num3){
+    console.log("primer numero es mayor ")
+    }else if (num2>num3 && num2>num1){
+    console.log(" segundo numero es mayor ")
     }else{
-        console.log(`es menor que 100 `)
-        console.log(`el numero esta entre 100`)
+      console.log(" el numero tres es mayor ")
     }
-    
 })
