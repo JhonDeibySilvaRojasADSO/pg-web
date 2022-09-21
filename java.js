3e30ae6333dc56306be7761c20aedad78e360a5d
JhonDeibySilvaRojasADSO <silvarojasjhondeiby@gmail.com>
2022-09-21 11:24:46 -0500

    ejercicio 6

--- java.js
+++ java.js
@@ -1,11 +1,19 @@
 addEventListener("DOMContentLoaded", ()=>{
-    //#5Escriba un algoritmo que calcule el área de un rectángulo siempre y cuando los lados sean positivos
-    //#entrada de datos 
-    let area=Number(prompt(" coloque el area de un triangulo:"))
-    if (area>0){
-      let res=(area*area)
-      console.log("el area de el rectangulo es de :",res)
-    }else{
-      console.log("escriba un valor positivo")
-    }
+//#6Considere dos variables llamadas temperatura y presión. Escriba una sentencia if-else que muestre en pantalla la palabra Alarma si la variable presión es mayor a 200 o si la variable temperatura es mayor a 100. En caso contrario, se debe mostrar en pantalla la palabra Normal.
+//#ENTRADA DE DATOS
+let temperatura=Number(prompt("coloque la temperatura"))
+let precion=Number(prompt("coloque la precion"))
+//#proceso
+if (precion>200){
+  console.log("ALERTA EN LA PRECION:",precion)
+
+}else{
+  console.log("NORMAL EN LA PRECION:",precion)
+
+
+}if (temperatura>100){
+  console.log("ALERTA EN LA TEMPERATURA:",temperatura)
+}else{
+  console.log("NORMALEN LA TEMPERATURA:",temperatura)
+}
 })
