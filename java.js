78f2853c29a90b600c0f815773f55178dbf24cca
JhonDeibySilvaRojasADSO <silvarojasjhondeiby@gmail.com>
2022-09-21 11:27:50 -0500

    ejercicio 7

--- java.js
+++ java.js
@@ -1,19 +1,15 @@
 addEventListener("DOMContentLoaded", ()=>{
-//#6Considere dos variables llamadas temperatura y presión. Escriba una sentencia if-else que muestre en pantalla la palabra Alarma si la variable presión es mayor a 200 o si la variable temperatura es mayor a 100. En caso contrario, se debe mostrar en pantalla la palabra Normal.
-//#ENTRADA DE DATOS
-let temperatura=Number(prompt("coloque la temperatura"))
-let precion=Number(prompt("coloque la precion"))
-//#proceso
-if (precion>200){
-  console.log("ALERTA EN LA PRECION:",precion)
-
-}else{
-  console.log("NORMAL EN LA PRECION:",precion)
-
-
-}if (temperatura>100){
-  console.log("ALERTA EN LA TEMPERATURA:",temperatura)
-}else{
-  console.log("NORMALEN LA TEMPERATURA:",temperatura)
-}
+    //#Calcular todos los pagos hechos de un restaurante y que si el consumo ingresado excede los$130.000 el descuento será del 15%, de lo contrario no hay descuento.
+    //#ENTRADA DE DATOS
+    let pago1=Number(prompt("dijite el valor total a pagar"))
+    let desc=0.15
+    //#proceso
+    if  (pago1 >=130000){
+      let res1=pago1*desc
+      let res2=pago1-res1
+      console.log("su valor total con el 15% de descuento es de:",res2)
+    }else{
+      console.log("no tiene el 15% de descuento ya que no cumplio con la cuota del restaurante")
+      console.log("el valor a pagar es de :",pago1)
+    }
 })
