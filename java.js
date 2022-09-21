1494490f139a59cceb1a925147c56cef5dd86283
JhonDeibySilvaRojasADSO <silvarojasjhondeiby@gmail.com>
2022-09-21 11:56:35 -0500

    ejercicio12

--- java.js
+++ java.js
@@ -1,21 +1,16 @@
 addEventListener("DOMContentLoaded", ()=>{
-    //#11Programa para solicitar el nombre, apellido, edad y la nota promedio de 5 estudiantes de un curso de computación.
-    //#ENTRADA DE DATOS
-    let nombre=prompt("ingrese su nombre:")
-    let apellido=prompt("ingrese su apellido:")
-    let edad=prompt("ingrese su eddad:")
-    
-    let not1=Number(prompt("dijite su nota 1:"))
-    let not2=Number(prompt("dijite su nota 2:"))
-    let not3=Number(prompt("dijite su nota 3:"))
-    let not4=Number(prompt("dijite su nota 4:"))
-    let not5=Number(prompt("dijite su nota 5:"))
-    
-    let prom=(not1+not2+not3+not4+not5)/5
-    if (prom >=5){
-      console.log("aprobaado")
-    }else{
-      console.log("no aprobado")
-    console.log("promedio de las notas :",prom)
-    }
+        //#12Restarle al primer número al segundo (siempre y cuando el primero sea mayor que él segundo, en caso contrario indicar con un mensaje que la operación no es posible realizarla.
+       // #ENTRADA DE DATOS
+        let num1=Number(prompt("Dijite el primer numero:"))
+        let num2 =Number(prompt("Dijte el segundo numero:"))
+        
+        //#proceso de datos
+        
+        if (num1>num2){
+          let res=num1-num2
+          console.log("se respuesta es :", res)
+        }else{
+            console.log("la operacion no es posible realizarla")
+          
+        }
 })
