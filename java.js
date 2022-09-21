53ae0a596e81bea3088f1d8723cbdb360e8da210
JhonDeibySilvaRojasADSO <silvarojasjhondeiby@gmail.com>
2022-09-21 11:11:05 -0500

    ejercicio 4

--- java.js
+++ java.js
@@ -1,30 +1,18 @@
 addEventListener("DOMContentLoaded", ()=>{
-    //#3Desarrollar un diagrama que lea 3 valores diferentes e indique cual es el mayor de ellos, el menor o si son iguales
-    //#entrada de datos
-    num1=Number(prompt("ingrese el pirmer numero:"))
-    num2=Number(prompt("ingrese el segundo numero:"))
-    num3=Number(prompt("ingrese el tercero numero:"))
-    //#proceso
-    //#esta parte es de mayor
-    if (num1>num2 && num1>num3){
-      console.log("El numero 1 es mayor")
-    }else if (num2>num3 && num2>num1){
-      console.log("El numero 2 es mayor")
-    }else{
-      console.log("el numero 3 es mayor")
-    //#esta parte es de menor
-    }if (num1<=num2 && num1<=num3){
-      console.log("El numero 1 es menor")
-    }else if (num2<=num3 && num2<=num1){
-      console.log("El numero 2 es menor")
-    }else{
-      console.log("el numero 3 es menor")
-    //#esata parte es de igual
-    }if (num1==num2){
-      console.log("El numero 1 y 2 son iguales ")
-    }else if (num2==num3){
-      console.log("El numero 2 y 3 son iguales")
-    }else if (num3==num1){
-      console.log("el numero 1 y 3 son iguales")
-    }
+//#4Cálculo de áreas - Elige una figura geométrica:" Triángulo y Círculo
+//#ENTRADA DE DATOS
+let figura=prompt("elija una figura triangulo(t) o circulo(c):")
+//#PROCESO
+if (figura== "t"){
+  let base=Number(prompt("coloque la base del  triangulo:"))
+  let altura=Number(prompt("coloque la altura del triangulo:"))
+  let res=(base*altura)/2
+  console.log("el area del triangulo es de:",res)
+
+}else if (figura=="c"){
+  let pi=3.14
+  let radio=Number(prompt("coloque el radio:"))
+  let res=(pi*radio)**2
+  console.log("el area del circulo es de:",res)
+}
 })
