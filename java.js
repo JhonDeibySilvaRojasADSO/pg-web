359ba0aafef7bae89a5be0f8ca082cce6f421da2
JhonDeibySilvaRojasADSO <silvarojasjhondeiby@gmail.com>
2022-09-21 11:16:50 -0500

    ejercicio 5

--- java.js
+++ java.js
@@ -1,18 +1,11 @@
 addEventListener("DOMContentLoaded", ()=>{
-//#4Cálculo de áreas - Elige una figura geométrica:" Triángulo y Círculo
-//#ENTRADA DE DATOS
-let figura=prompt("elija una figura triangulo(t) o circulo(c):")
-//#PROCESO
-if (figura== "t"){
-  let base=Number(prompt("coloque la base del  triangulo:"))
-  let altura=Number(prompt("coloque la altura del triangulo:"))
-  let res=(base*altura)/2
-  console.log("el area del triangulo es de:",res)
-
-}else if (figura=="c"){
-  let pi=3.14
-  let radio=Number(prompt("coloque el radio:"))
-  let res=(pi*radio)**2
-  console.log("el area del circulo es de:",res)
-}
+    //#5Escriba un algoritmo que calcule el área de un rectángulo siempre y cuando los lados sean positivos
+    //#entrada de datos 
+    let area=Number(prompt(" coloque el area de un triangulo:"))
+    if (area>0){
+      let res=(area*area)
+      console.log("el area de el rectangulo es de :",res)
+    }else{
+      console.log("escriba un valor positivo")
+    }
 })
