28a4a4b6510923109e3267ac948b0ae5aaeb9542
JhonDeibySilvaRojasADSO <silvarojasjhondeiby@gmail.com>
2022-09-21 11:37:12 -0500

    ejercicio 9

--- java.js
+++ java.js
@@ -1,5 +1,22 @@
 addEventListener("DOMContentLoaded", ()=>{
-    //#8En cierta empresa se les paga a sus trabajadores de la siguiente forma: si el empleado es de planta, la hora trabajada se le paga a $20000, si el empleado es administrativo, la hora trabajada se le paga a $10000. Para calcular su pago es necesario conocer el total de horas trabajadas.
+    // DEFINIR VARIABLES NUEMRIAS
+// DATOS DE ENTRADA
+    // OPERACIONES BASICAS: + /*
+    let prod1=Number(prompt(`coloque el valor del producto:`))
+    let prod2=Number(prompt(`coloque el valor del producto:`))
+    let prod3=Number(prompt(`coloque el valor del producto:`))
+    let prod4=Number(prompt(`coloque el valor del producto:`))
+    let prod5=Number(prompt(`coloque el valor del producto:`))
+    
+    //PROCESOS U OPERACIONES
+    let tol=(prod1+prod2+prod3+prod4+prod5)
+    let iva=Number(prompt(`coloque el valor del iva`))
+    let vaiva=tol*iva
+    let res=tol+vaiva
+    //salida o entrada de resultado
+    console-log(`valor del producto con el iva:, ${res}`)
+    console-log(`el sub valor es de :,${tol}`)
+    console-log(`valor del iva:, ${vaiva}`)//#8En cierta empresa se les paga a sus trabajadores de la siguiente forma: si el empleado es de planta, la hora trabajada se le paga a $20000, si el empleado es administrativo, la hora trabajada se le paga a $10000. Para calcular su pago es necesario conocer el total de horas trabajadas.
     //#ENTRADA DE DATOS
     let trabajador=prompt("dijite la (p) si es trabajador de planta, dijite la (a) si es trabajador administrativo:")
     //#proceso
