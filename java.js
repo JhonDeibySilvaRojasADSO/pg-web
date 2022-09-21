2d6063584cf4d774d0b959aa5c67ea55df1d67ff
JhonDeibySilvaRojasADSO <silvarojasjhondeiby@gmail.com>
2022-09-21 12:05:19 -0500

    ejercicio 13

--- java.js
+++ java.js
@@ -1,16 +1,22 @@
 addEventListener("DOMContentLoaded", ()=>{
-        //#12Restarle al primer número al segundo (siempre y cuando el primero sea mayor que él segundo, en caso contrario indicar con un mensaje que la operación no es posible realizarla.
-       // #ENTRADA DE DATOS
-        let num1=Number(prompt("Dijite el primer numero:"))
-        let num2 =Number(prompt("Dijte el segundo numero:"))
-        
-        //#proceso de datos
-        
-        if (num1>num2){
-          let res=num1-num2
-          console.log("se respuesta es :", res)
-        }else{
-            console.log("la operacion no es posible realizarla")
-          
-        }
+    //#13En una tienda de HELADO da un descuento por compra a sus clientes con membresía dependiendo de su tipo, sólo existen tres tipos de membresía, tipo A, tipo B y tipo C. Los descuentos son los siguientes: Tipo A 10% de descuento Tipo B 15% de descuento Tipo C 20% de descuento
+    //#ENTRADA DE DATOS
+    let men=prompt("Dijite cual es su tipo de membresia A, B, C, D:")
+    let num1=Number(prompt("dijite el precio del helado:"))
+    //#proceso de datos
+    if (men==("A")){
+      let res=num1*0.10
+      let res2=num1-res
+      console.log("su valordel helado es de:",res2,"con el descuento de la membracia tipo A")
+    }else if (men==("B")){
+      let res=num1*0.15
+      let res2=num1-res
+      console.log("su valordel helado es de:",res2,"con el descuento de la membracia tipo B")
+    }else if (men== ("C")){
+      let res=num1*0.20
+      let res2=num1-res
+      console.log("su valordel helado es de:",res2,"con el descuento de la membracia tipo C")
+    }else if (men==("D")){
+      console.log(" su valor del helado es de :" ,num1, " sin nungun descuento")
+    }
 })
